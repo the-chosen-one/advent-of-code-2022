b6b16a3448d21d5cf85147a2fe775efcaab1c149
Simbarashe Musarurwa <simba@dialexa.com>
2022-12-04 12:49:23 -0600

    Day 1 Solution

---
+++ Day1/solution.ex
@@ -0,0 +1,16 @@
+listOfCalories = input
+|> String.split("\n\n")
+|> Enum.map(fn calories ->
+  String.split(calories)
+  |> Enum.map(fn calorie -> String.to_integer(calorie) end)
+  |> Enum.sum end)
+
+maxCalories = Enum.max(listOfCalories)
+
+sumOfTopThree = listOfCalories
+|> Enum.sort(&(&1 >= &2))
+|> Enum.take(3)
+|> Enum.sum()
+
+IO.puts "The Max is #{maxCalories} "
+IO.puts "The Sum of top 3 is #{sumOfTopThree} "
